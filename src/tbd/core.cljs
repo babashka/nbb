023e2f79565c76e6918e19a2b5edff2ed8fcc57f
Michiel Borkent <michielborkent@gmail.com>
2021-07-29 15:37:22 +0200

    wip

--- src/tbd/core.cljs
+++ src/tbd/core.cljs
@@ -7,20 +7,21 @@
 (def cwd (.cwd js/process))
 
 ;; hack from  https://swizec.com/blog/making-a-node-cli-both-global-and-local/
-(defn patched-require [s]
-  (if (str/starts-with? s ".")
-    (js/require (str/join "/" [cwd s]))
-    (let [path (str/join "/" [cwd "node_modules" s])]
-      (try (js/require path)
-           (catch :default _e
-             (js/require s))))))
+(let [normal-require js/require]
+  (defn patched-require [s]
+    (if (str/starts-with? s ".")
+      (normal-require (str/join "/" [cwd s]))
+      (let [path (str/join "/" [cwd "node_modules" s])]
+        (try (normal-require path)
+             (catch :default _e
+               (normal-require s)))))))
 
 (set! (.-require universe) patched-require)
 
 (def sci-ctx (atom (sci/init {:namespaces {'clojure.core {'prn prn 'println println}}
                               :classes {'js universe :allow :all}})))
 
-(defn eval! [code]
+(defn eval-code [code]
   (let [reader (sci/reader code)]
     (try
       (loop [result nil]
@@ -35,3 +36,5 @@
 (defn register-plugin! [plug-in-name sci-opts]
   plug-in-name ;; unused for now
   (swap! sci-ctx sci/merge-opts sci-opts))
+
+(prn :dude)
--- src/tbd/main.cljs
+++ src/tbd/main.cljs
@@ -3,7 +3,10 @@
 
 (def fs (js/require "fs"))
 
+(prn :pre-main)
+
 (defn main [& [script-file]]
+  (prn :script-file script-file)
   (let [source (str (.readFileSync fs script-file))]
-    (tbd/eval! source)))
+    (tbd/eval-code source)))
 
