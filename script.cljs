9f0260cfaa2785710261645a55ac0c58648895f8
Michiel Borkent <michielborkent@gmail.com>
2021-08-01 18:34:37 +0200

    Update test script

--- script.cljs
+++ script.cljs
@@ -1,8 +1,10 @@
 (ns script
+  #_"This is a smoke test, we have to turn this into real tests."
   (:require
    ["csv-parse/lib/sync" :as csv-parse]
    ["fs" :as fs]
-   ["shelljs" :as sh]))
+   ["shelljs" :as sh]
+   [clojure.string :as str]))
 
 (println (count (str (.readFileSync fs "script.cljs"))))
 (println (count (str (fs/readFileSync "script.cljs"))))
@@ -13,3 +15,4 @@
 
 (require '[reagent.core :as rg])
 (prn (some? rg/as-element))
+(prn (str/includes? "foo" "o"))
