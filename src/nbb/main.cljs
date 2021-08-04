e116793fd7136df7ad247ce2a602ff693d8c0a3c
Michiel Borkent <michielborkent@gmail.com>
2021-08-04 12:31:55 +0200

    Dynamically load

--- src/nbb/main.cljs
+++ src/nbb/main.cljs
@@ -3,29 +3,20 @@
             ["module" :as mod1 :refer [createRequire]]
             ["path" :as path]
             [nbb.core :as nbb]
-            [sci.core :as sci]
-            [shadow.esm :as esm]))
+            [sci.core :as sci]))
 
 (defn main []
-  
   (let [[_ _ script-file] js/process.argv
         path (when script-file (path/resolve script-file))
-        _ (prn :path path)
         require (when path
                   (createRequire path))]
-    (.then (esm/dynamic-import "import-meta-resolve")
-           (fn [mod]
-             (let [resolve (.-resolve mod)]
-               (.then (resolve "react" (str "file://" path))
-                      (fn [resolved]
-                        (prn :res resolved)
-                        (esm/dynamic-import resolved))))))
-    #_#_(when require
+    (when require
       (set! (.-require goog/global) require))
     (if script-file
       (let [source (str (fs/readFileSync script-file))]
         ;; NOTE: binding doesn't work as expected since eval-code is async.
         ;; Since nbb currently is only called with a script file argument, this suffices
         (sci/alter-var-root nbb/command-line-args (constantly (seq (js/process.argv.slice 3))))
-        (nbb/eval-code source require))
-      (.error js/console "Nbb expects a script file argument.")) ))
+        (nbb/eval-code {:require require
+                        :script-dir path} source))
+      (.error js/console "Nbb expects a script file argument."))))
