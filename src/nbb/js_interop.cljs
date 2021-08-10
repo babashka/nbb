b6a384737ef9dc334d10d12f02584f5f1fc14b2c
Michiel Borkent <michielborkent@gmail.com>
2021-08-10 16:07:42 +0200

    wip

--- src/nbb/js_interop.cljs
+++ src/nbb/js_interop.cljs
@@ -11,7 +11,19 @@
    'get-in (sci/copy-var j/get-in jns)
    'contains? (sci/copy-var j/contains? jns)
    'select-keys (sci/copy-var j/select-keys jns)
-   'lookup (sci/copy-var j/lookup jns)})
+   'lookup (sci/copy-var j/lookup jns)
+   'assoc! (sci/copy-var j/assoc! jns)
+   'assoc-in! (sci/copy-var j/assoc-in! jns)
+   'update! (sci/copy-var j/update! jns)
+   'update-in! (sci/copy-var j/update-in! jns)
+   'extend! (sci/copy-var j/extend! jns)
+   'push! (sci/copy-var j/push! jns)
+   'unshift! (sci/copy-var j/unshift! jns)
+   'call (sci/copy-var j/call jns)
+   'apply (sci/copy-var j/apply jns)
+   'call-in (sci/copy-var j/call-in jns)
+   'apply-in (sci/copy-var j/apply-in jns)
+   'obj (sci/copy-var j/obj jns)})
 
 (defn init []
   (nbb/register-plugin!
