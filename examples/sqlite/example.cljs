dcb754e33be21ba0546b8349c55cc17ee5a80e81
Michiel Borkent <michielborkent@gmail.com>
2021-09-16 12:30:16 +0200

    Simplify

--- examples/sqlite/example.cljs
+++ examples/sqlite/example.cljs
@@ -7,10 +7,10 @@
 (p/let [db (open #js {:filename ":memory:"
                       :driver sqlite/Database})
         _ (.run db "CREATE TABLE lorem (info TEXT)")
-        stmt (.prepare db "INSERT INTO lorem VALUES (?)")
-        _ (p/all (map #(.run stmt (str %)) (range 10)))
-        _ (.finalize stmt)
-        _ (.each db "SELECT rowid AS id, info FROM lorem"
-                 (fn [_err row]
-                   (println (str (.-id row) ": " (.-info row)))))]
+        stmt (.prepare db "INSERT INTO lorem VALUES (?)")]
+  (p/all (map #(.run stmt (str %)) (range 10)))
+  (.finalize stmt)
+  (.each db "SELECT rowid AS id, info FROM lorem"
+           (fn [_err row]
+             (println (str (.-id row) ": " (.-info row)))))
   (.close db))
