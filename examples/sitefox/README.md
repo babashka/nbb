2e649017d4c1a4309f805dbda5c0d1efc5fe2ef9
Michiel Borkent <michielborkent@gmail.com>
2021-10-09 12:48:11 +0200

    README [skip ci]

--- examples/sitefox/README.md
+++ examples/sitefox/README.md
@@ -6,3 +6,7 @@ To start the web server, first run `npm install` and then `npm run fox` (or `bb
 fox` if you have [babashka](https://github.com/babashka/babashka) installed).
 
 This example requires `nbb` v0.0.97 or higher.
+
+See it in action [here](https://twitter.com/borkdude/status/1446789040188051463).
+
+To develop with nREPL, run `bb dev`.
--- examples/sitefox/webserver.cljs
+++ examples/sitefox/webserver.cljs
@@ -1,4 +1,6 @@
 (ns webserver
+  {:clj-kondo/config '{:lint-as {promesa.core/let clojure.core/let
+                                 webserver/defp clojure.core/def}}}
   (:require
    [promesa.core :as p]
    [reagent.dom.server :refer [render-to-static-markup] :rename {render-to-static-markup r}]
@@ -6,9 +8,25 @@
 
 (defn setup-routes [app]
   (web/reset-routes app)
-  (.get app "/" (fn [_req res] (.send res (r [:h1 "Hello world!"])))))
+  (.get app "/"
+        (fn [_req res] (.send res (r [:h1 "Hello world!"])))))
 
 (p/let [app (web/create)
         [_host _port] (web/serve app)]
   (setup-routes app)
   (println "Serving."))
+
+;;;; Scratch
+
+(comment
+  (defmacro defp [binding expr]
+    `(let [v# ~expr]
+       (if (instance? js/Promise v#)
+         (.then (fn [val]
+                  (def ~binding val)))
+         (def ~binding v#))))
+
+  (defp app (web/create))
+  (web/serve app)
+  (setup-routes app)
+  )
