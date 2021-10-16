b3c4fce999ed272d5d2a3e0a38d94d03bb312cb2
Michiel Borkent <michielborkent@gmail.com>
2021-10-16 11:35:33 +0200

    clean

--- src/nbb/reagent.cljs
+++ src/nbb/reagent.cljs
@@ -62,9 +62,6 @@
 
 (def rns (sci/create-ns 'reagent.core nil))
 
-(defn r-create-class [spec]
-  (reagent.core/create-class spec))
-
 (def reagent-namespace
   {'atom (sci/copy-var r/atom rns)
    'as-element (sci/copy-var r/as-element rns)
