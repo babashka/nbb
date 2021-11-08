b9318536f78437a05255d804e9b587926661b3b4
Michiel Borkent <michielborkent@gmail.com>
2021-11-08 18:28:33 +0100

    Changelogs

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+For a list of breaking changes, check [here](#breaking-changes).
+
+## 0.0.108
+
+- Support `test-vars` and `use-fixtures` in `clojure.test` namespace
+  [#99](https://github.com/babashka/nbb/issues/99)
+
+## Breaking changes
+
+None yet.
---
+++ script/changelog.clj
@@ -0,0 +1,22 @@
+#!/usr/bin/env bb
+
+(ns changelog
+  (:require [clojure.string :as str]))
+
+(let [changelog (slurp "CHANGELOG.md")
+      replaced (str/replace changelog
+                            #" #(\d+)"
+                            (fn [[_ issue after]]
+                              (format " [#%s](https://github.com/babashka/nbb/issues/%s)%s"
+                                      issue issue (str after))))
+      replaced (str/replace replaced
+                            #" borkdude/sci#(\d+)"
+                            (fn [[_ issue after]]
+                              (format " [borkdude/sci#%s](https://github.com/borkdude/sci/issues/%s)%s"
+                                      issue issue (str after))))
+      replaced (str/replace replaced
+                            #"@([a-zA-Z0-9-_]+)([, \.)])"
+                            (fn [[_ name after]]
+                              (format "[@%s](https://github.com/%s)%s"
+                                      name name after)))]
+  (spit "CHANGELOG.md" replaced))
