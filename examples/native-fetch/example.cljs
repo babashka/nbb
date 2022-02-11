65e0dc8bbb2ed31c14b9ad56a10c30f5a44ac126
Michael Salihi <admin@prestance-informatique.fr>
2022-02-11 10:53:58 +0100

    Keep native fetch example simple (#125)

--- examples/native-fetch/example.cljs
+++ examples/native-fetch/example.cljs
@@ -1,9 +1,8 @@
 (ns example
-  (:require [promesa.core :as p]
-            [applied-science.js-interop :as j]))
+  (:require [promesa.core :as p]))
 
 (p/let [response (js/fetch "https://clojure.org")
-        status (j/get response :status)
+        status (.-status response)
         body (.text response)]
   (prn status) ;; => 200
   (prn (subs body 0 10))) ;; => "<!DOCTYPE "
