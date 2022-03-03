b31e1fda997e385a0f33642da3ea72a0483cb1de
Michiel Borkent <michielborkent@gmail.com>
2022-03-03 18:37:24 +0100

    reagent dom server

--- src/nbb/impl/reagent/dom/server.cljs
+++ src/nbb/impl/reagent/dom/server.cljs
@@ -1,16 +1,9 @@
 (ns nbb.impl.reagent.dom.server
   (:require
    [nbb.core :as nbb]
-   [reagent.dom.server :as srv]
-   [sci.core :as sci]))
-
-(def rdns (sci/create-ns 'reagent.dom.server nil))
-
-(def reagent-dom-server-namespace
-  {'render-to-string (sci/copy-var srv/render-to-string rdns)
-   'render-to-static-markup (sci/copy-var srv/render-to-static-markup rdns)})
+   [sci-configs.reagent.reagent-dom-server :as srv]))
 
 (defn init []
   (nbb/register-plugin!
    ::reagent-dom-server
-   {:namespaces {'reagent.dom.server reagent-dom-server-namespace}}))
+   {:namespaces {'reagent.dom.server srv/reagent-dom-server-namespace}}))
