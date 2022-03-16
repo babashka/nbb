bafcbb4dd0bf6761f01e9fcc15cb6c000d87c947
Michiel Borkent <michielborkent@gmail.com>
2022-03-16 10:31:17 +0100

    Add output [skip ci]

--- examples/node-ical/example.cljs
+++ examples/node-ical/example.cljs
@@ -18,3 +18,15 @@ END:VEVENT
 END:VCALENDAR")))
 
 (pprint direct-events)
+#_
+{"7014-1567468800-1567555199@peterbraden@peterbraden.co.uk"
+ {"params" [],
+  "uid" "7014-1567468800-1567555199@peterbraden@peterbraden.co.uk",
+  "datetype" "date-time",
+  "summary" "Hey look! An example event!",
+  "location" "1000 Broadway Ave., Brooklyn",
+  "status" "CONFIRMED",
+  "start" #inst "2013-08-02T14:34:00.000-00:00",
+  "type" "VEVENT",
+  "end" #inst "2013-08-02T15:04:00.000-00:00",
+  "description" " Do something in NY."}}
