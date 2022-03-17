85449a248d2fa6bfcb5970a8df05f12c150c0c61
Michiel Borkent <michielborkent@gmail.com>
2022-03-17 13:50:16 +0100

    fix #146: nREPL completions (#147)

--- script/nbb_nrepl_tests.clj
+++ script/nbb_nrepl_tests.clj
@@ -163,6 +163,36 @@
       (bencode/write-bencode os {"op" "eval" "code" "(js/process.exit 0)"
                                  "session" session "id" (new-id!)}))))
 
+(deftest complete-test
+  (swap! port inc)
+  (nrepl-server)
+  (wait-for-port "localhost" @port)
+  (with-open [socket (Socket. "127.0.0.1" @port)
+              in (.getInputStream socket)
+              in (java.io.PushbackInputStream. in)
+              os (.getOutputStream socket)]
+    (bencode/write-bencode os {"op" "clone"})
+    (let [session (:new-session (read-msg (bencode/read-bencode in)))
+          id (atom 0)
+          new-id! #(swap! id inc)]
+      (testing "complete"
+        (bencode/write-bencode os {"op" "eval" "code" "(require '[nbb.core :as nbb])"
+                                   "session" session "id" (new-id!)})
+        (let [_ (read-reply in session @id)
+              msg (read-reply in session @id)
+              status (:status msg)
+              _ (is (= ["done"] status))])
+       (bencode/write-bencode os
+                              {"op" "complete" "symbol" "nbb/"
+                                "session" session "id" (new-id!)})
+        (let [msg (read-reply in session @id)
+              completions (:completions msg)
+              completions (set (map read-msg completions))]
+          (is (contains? completions {:candidate "nbb/load-string", :ns "nbb.core"}))
+          (is (contains? completions {:candidate "nbb/await",       :ns "nbb.core"}))))
+      (bencode/write-bencode os {"op" "eval" "code" "(js/process.exit 0)"
+                                 "session" session "id" (new-id!)}))))
+
 (defn -main [& _]
   (let [{:keys [:error :fail]} (t/run-tests 'nbb-nrepl-tests)]
     (when (pos? (+ error fail))
