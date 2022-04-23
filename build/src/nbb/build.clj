0719a0c4de64e69525da453f03622a8be5cdc65d
Gabriel Horner <gabriel@logseq.com>
2022-04-22 23:57:27 -0400

    Finish converting remaining tasks to build ns

--- build/src/nbb/build.clj
+++ build/src/nbb/build.clj
@@ -1,4 +1,5 @@
 (ns nbb.build
+  "Provides bb tasks for building and releasing nbb"
   (:require [babashka.fs :as fs]
             [babashka.classpath :as classpath]
             [babashka.tasks :refer [shell clojure]]
@@ -12,15 +13,15 @@
                  (fs/file d "nbb_features.edn"))
                (classpath/split-classpath (classpath/get-classpath)))))
 
-(defn read-configs
+(defn- read-configs
   [files]
   (->> files
        (mapcat (comp edn/read-string slurp str))))
 
-(defn- wrap-cmd [cmd]
+(defn- build-cmd [cmd]
   (let [files (feature-files)
         feature-configs (read-configs files)
-        ;; Assume feature ./src/nbb_features.edn has a ./deps.edn
+        ;; Each ./src/nbb_features.edn has a ./deps.edn
         feature-dirs (map (comp fs/parent fs/parent) files)
         cmd (if (seq files)
               (format "-Sdeps '%s' %s"
@@ -32,7 +33,6 @@
                                   feature-dirs))}
                       cmd)
               cmd)]
-    ;; TODO: Add back *test*
     (if (seq feature-configs)
       (apply str cmd
         (map (fn [m] (format " --config-merge '%s'" (pr-str (:shadow-config m))))
@@ -40,17 +40,18 @@
       cmd)))
 
 (defn build
-  "Build nbb_core.js using given clojure args and commandline args"
+  "Build nbb shadow builds using clojure cmd and commandline args. Features on
+  classpath are automatically added"
   [cmd args]
-  (apply clojure (wrap-cmd cmd) args))
+  (apply clojure (build-cmd cmd) args))
 
 (defn release
   "Compiles release build."
-  []
-  (build "-M -m shadow.cljs.devtools.cli --force-spawn release modules"
-         *command-line-args*)
+  [args & {:keys [wrap-cmd-fn] :or {wrap-cmd-fn identity}}]
+  (build (wrap-cmd-fn "-M -m shadow.cljs.devtools.cli --force-spawn release modules")
+         args)
   (spit "lib/nbb_core.js"
-        (clojure.string/replace (slurp "lib/nbb_core.js") (re-pattern "self") "globalThis"))
+        (str/replace (slurp "lib/nbb_core.js") (re-pattern "self") "globalThis"))
   (spit "lib/nbb_main.js"
         (str "#!/usr/bin/env node\n\n" (slurp "lib/nbb_main.js")))
   (shell "chmod +x lib/nbb_main.js")
