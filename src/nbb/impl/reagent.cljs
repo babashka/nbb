5696185435b0d59580e6d985ce4699c2a2b583e3
Michiel Borkent <michielborkent@gmail.com>
2022-04-29 12:44:20 +0200

    Add reactify-component

--- src/nbb/impl/reagent.cljs
+++ src/nbb/impl/reagent.cljs
@@ -1,6 +1,7 @@
 (ns nbb.impl.reagent
   (:require
    [nbb.core :as nbb]
+   [reagent.core :as re]
    [reagent.ratom]
    [sci-configs.reagent.reagent :as r]
    [sci.core :as sci]))
@@ -10,7 +11,8 @@
 (defn init []
   (nbb/register-plugin!
    ::reagent
-   {:namespaces {'reagent.core r/reagent-namespace
+   {:namespaces {'reagent.core (assoc r/reagent-namespace
+                                      'reactify-component (sci/copy-var re/reactify-component rns))
                  'reagent.ratom (assoc r/reagent-ratom-namespace
                                        ;; TODO: move the below to sci configs
                                        'atom (sci/copy-var reagent.ratom/atom
