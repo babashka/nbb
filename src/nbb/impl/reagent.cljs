c6b8661e5df8bea8c3022349c854b975c88a4545
Michiel Borkent <michielborkent@gmail.com>
2022-06-12 21:47:04 +0200

    Fix #214: add reagent.core/adapt-react-class

--- src/nbb/impl/reagent.cljs
+++ src/nbb/impl/reagent.cljs
@@ -1,15 +1,15 @@
 (ns nbb.impl.reagent
   (:require
    [nbb.core :as nbb]
+   [reagent.core :as reagent]
    [reagent.ratom]
    [sci.configs.reagent.reagent :as r]
    [sci.core :as sci]))
 
-(def rns (sci/create-ns 'reagent.ratom))
-
 (defn init []
   (nbb/register-plugin!
    ::reagent
-   {:namespaces {'reagent.core r/reagent-namespace
+   {:namespaces {'reagent.core (assoc r/reagent-namespace 'adapt-react-class
+                                      (sci/copy-var reagent/adapt-react-class (sci/create-ns 'reagent.core)))
                  'reagent.ratom r/reagent-ratom-namespace
                  'reagent.debug r/reagent-debug-namespace}}))
