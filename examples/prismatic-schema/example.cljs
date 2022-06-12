2cb84869ed2110b387568ea95618a55261d7ba0b
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-06-12 17:22:54 -0300

    feat: prismatic-schema code sample

--- examples/prismatic-schema/example.cljs
+++ examples/prismatic-schema/example.cljs
@@ -1,7 +1,7 @@
 (ns example
-  [schema.core :as s :include-macros true])
+  (:require [schema.core :as s :include-macros true]))
 
 (s/set-fn-validation! true)
 (s/validate {:a s/Int} {:a 1})
 ; This will make things break:
-; (s/validate {:a s/Int} {:a "2"})
+(s/validate {:a s/Int} {:a "2"})
