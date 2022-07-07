876727ac927c2b6ed34dea8a027f0549f7f47e85
Michiel Borkent <michielborkent@gmail.com>
2022-07-07 14:31:14 +0200

    wip [skip ci]

--- test-scripts/bundler/example.cljs
+++ test-scripts/bundler/example.cljs
@@ -1,10 +1,13 @@
 (ns example
   (:require
    ["chalk$default" :as chalk]
-   [another-namespace :as another]))
+   [another-namespace :as another]
+   [promesa.core :as p]))
 
 (def log js/console.log)
 
 (log (chalk/blue "hello"))
 (log (another/cool-fn))
 
+(p/-> (p/delay 1000 :hello)
+      prn)
--- test-scripts/bundler/out.mjs
+++ test-scripts/bundler/out.mjs
@@ -1,5 +1,8 @@
 
 import { loadFile, registerModule } from '../../lib/nbb_api.js'
+
+import '../../lib/nbb_promesa.js'
+
 import * as nbb_dot_internal_dot_chalk from 'chalk'
 registerModule(nbb_dot_internal_dot_chalk, 'chalk')
 import * as foo from 'term-size'
