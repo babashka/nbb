cb5485304d7dbd2596167552494e491d80f3909b
Michiel Borkent <michielborkent@gmail.com>
2022-07-16 11:22:06 +0200

    goog.string

--- doc/bundle/example.cljs
+++ doc/bundle/example.cljs
@@ -3,6 +3,7 @@
    ["chalk$default" :as chalk]
    [another-namespace :as another]
    [from-classpath :as fc]
+   [goog.string :as gstring]
    [promesa.core :as p]
    [utils :as u]))
 
@@ -11,6 +12,6 @@
 (log (chalk/blue "hello"))
 (prn (fc/from-classpath))
 (prn (another/cool-fn))
-
+(prn (gstring/format "Awesome formatted: (%s)" "Yes!"))
 (p/-> (p/delay 1000 (u/util-fn))
       prn)
