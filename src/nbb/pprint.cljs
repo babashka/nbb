da54da2642e00e8bf6b5de6a68bbb40e0f616ee7
Michiel Borkent <michielborkent@gmail.com>
2022-08-31 12:03:29 +0200

    Moved pprint config to sci.configs

--- src/nbb/pprint.cljs
+++ src/nbb/pprint.cljs
@@ -1,23 +1,9 @@
 (ns nbb.pprint
-  (:require [cljs.pprint :as pp]
-            [nbb.core :as nbb]
-            [sci.core :as sci]))
-
-(def pns (sci/create-ns 'cljs.pprint nil))
-
-(defn pprint [& args]
-  (binding [*print-fn* @sci/print-fn]
-    (apply pp/pprint args)))
-
-(defn print-table [& args]
-  (binding [*print-fn* @sci/print-fn]
-    (apply pp/print-table args)))
-
-(def pprint-namespace
-  {'pprint (sci/copy-var pprint pns)
-   'print-table (sci/copy-var print-table pns)})
+  (:require
+   [nbb.core :as nbb]
+   [sci.configs.cljs.pprint :refer [config]]))
 
 (defn init []
   (nbb/register-plugin!
    ::pprint
-   {:namespaces {'cljs.pprint pprint-namespace}}))
+   config))
