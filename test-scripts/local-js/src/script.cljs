45173a21af9fff4d2235e8dc7cc69ee71b5c68a5
Michiel Borkent <michielborkent@gmail.com>
2022-10-09 16:51:35 +0200

    add test

--- test-scripts/local-js/src/script.cljs
+++ test-scripts/local-js/src/script.cljs
@@ -1,8 +1,10 @@
 (ns script
   (:require
+   ["./async_require.js" :refer [res1 res2 res3 res4]]
    ["./foo.mjs$default" :as foo]
    [other.script :as o]))
 
 (defn -main [& _]
   (assoc (js->clj foo :keywordize-keys true)
-         :other.script/foo o/foo))
+         :other.script/foo o/foo
+         :nss [res1 res2 res3 res4]))
