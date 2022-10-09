a211a54e733dbf75f107d288c3e9705dde8b8b7f
Michiel Borkent <michielborkent@gmail.com>
2022-10-09 16:28:39 +0200

    add JS test

--- scratch.js
+++
@@ -1,8 +0,0 @@
-import { loadString } from './index.mjs';
-
-const res2 = await loadString("(ns bar) (str (ns-name *ns*))");
-
-const res3 = await loadString("(str (ns-name *ns*))");
-
-console.log(await res2); // bar
-console.log(await res3); // bar, but should be user
