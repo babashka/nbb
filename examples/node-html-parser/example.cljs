ebeb01082c42286165491c92284484c1af038f4e
Michiel Borkent <michielborkent@gmail.com>
2022-10-14 19:43:16 +0200

    simplify

--- examples/node-html-parser/example.cljs
+++ examples/node-html-parser/example.cljs
@@ -1,23 +1,23 @@
 (ns example
   (:require
    ["node-html-parser" :as html :refer [parse]]
-   [clojure.string :as str]
-   [clojure.walk :as walk]))
+   [clojure.pprint :refer [pprint]]
+   [clojure.string :as str]))
 
 (def root (parse "<ul id=\"list\" ><li>Hello World</li></ul>"))
 
-(defn html->hiccup [root]
-  (walk/prewalk
-   (fn [elt]
-     (cond
-       (instance? html/HTMLElement elt)
-       (into (cond-> [(keyword (str/lower-case (str (.-tagName elt))))]
-               (pos? (count (js/Object.values (.-attributes elt))))
-               (conj (js->clj (.-attributes elt) :keywordize-keys true))) (.-childNodes elt))
-       (instance? html/TextNode elt)
-       (.-text elt)
-       :else
-       elt))
-   (.-firstChild root)))
+(defn html->hiccup [elt]
+  (cond
+    (instance? html/HTMLElement elt)
+    (into (cond-> [(keyword (str/lower-case (str (.-tagName elt))))]
+            (pos? (count (js/Object.values (.-attributes elt))))
+            (conj (js->clj (.-attributes elt) :keywordize-keys true)))
+          (map html->hiccup (.-childNodes elt)))
+    (instance? html/TextNode elt)
+    (.-text elt)
+    :else
+    (do
+      (prn elt ((js/eval "(x) => typeof(x)" elt)))
+      elt)))
 
-(prn (html->hiccup root))
+(run! pprint (map html->hiccup (.-childNodes root)))
